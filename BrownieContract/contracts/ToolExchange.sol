645a72a7192773e95e659a86b8df7e8e397cc48d
idea2547 <hahidea2547@gmail.com>
2021-11-30 12:34:11 +0700

    Update Ticket Exchange contract

--- BrownieContract/contracts/ToolExchange.sol
+++ BrownieContract/contracts/ToolExchange.sol
@@ -11,118 +11,52 @@ For transferFrom to succeed, sender must
 have more than amount tokens in their balance
 allowed TokenSwap to withdraw amount tokens by calling appro */
 
-contract TokenSwap is Ownable{
-    IERC20 public token;
-    address public owner1;
-    uint public amount1;
-    IERC20 public token2;
-    address public owner2;
-    uint public amount2;
-    IERC1155 public nftToken;
+contract ACMTokenSwap is Ownable{
+    IERC20 public ACMtoken;
+    IERC1155 public ACMnftToken;
+    address public owner;
+    uint public amount;
+    
 
-    uint256[] public allowTokenId;
+    mapping(uint256 => bool) public allowTokenId;
+    mapping(uint256 => uint256) public tokenPrice; 
 
     constructor(
         address _token,
-        address _owner1,
-        uint _amount1,
-        address _token2,
-        address _owner2,
-        uint _amount2,
+        address _owner,
+        uint _amount,
         address _nftToken,
-        uint256[] _allowTokenId
+        uint256[] _allowTokenId,
+        uint256[] _tokenPrice
     ) 
     {
-        token = IERC20(_token);
-        owner1 = _owner1;
-        amount1 = _amount1;
-        token2 = IERC20(_token2);
-        owner2 = _owner2;
-        amount2 = _amount2;
-        nftToken = _nftToken;
-        allowTokenId = _allowTokenId;
+        ACMtoken = IERC20(_token);
+        owner = _owner;
+        amount = _amount;
+        ACMnftToken = IERC1155(_nftToken);
+        for(i=0;i<_allowTokenId.length;i++){
+            allowTokenId[_allowTokenId[i]] = true;
+        }
     }
 
-    function setAllowTokenId(uint256[] arr) public onlyOwner{
-        allowTokenId = arr;
+    function setAllowTokenId(uint256 _tokenId, bool _bool) public onlyOwner{
+        allowTokenId[_tokenId] = _bool;
+    }
+    function setTokenPrice(uint256[] _tokenId, uint256 price) public onlyOwner{
+        tokenPrice[_tokenId] = price;
     }
 
 
     function buyTool(uint256 tokenId, uint256 amount) public {
-        IERC1155 acmNFTContract = IERC1155(nftToken);
-        IERC20 acmToken = IERC20(token); 
-        //balance Token > require
-        require(acmToken.balanceOf(msg.sender) > 0);
-        //token allowance this address >= amount to pay
-        //acmNFTContract Owner address need to have token > 0
-        require(tokenId )
+        require(ACMtoken.balanceOf(msg.sender) > 0);
+        require(allowTokenId[tokenId] == true, "This tokenId were not allow to purchase");
+        require(tokenPrice[tokenId] != 0, "Price of this token have not been set yet");
+        require(ACMToken.allowance(owner1, address(this)) >= amount, "Please allow the contract to spend your token");
+        require(ACMnftToken.isApprovedForAll(msg.sender, address(this)), "contract must approve");
 
-        uint256 amountTobuy = msg.value;
-        uint256 dexBalance = token.balanceOf(address(this));
-        require(amountTobuy > 0, "You need to send some ether");
-        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
-        token.transfer(msg.sender, amountTobuy);
-
-        
-        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
-        require(
-            token1.allowance(owner1, address(this)) >= amount1,
-            "Token 1 allowance too low"
-        );
-        require(
-            token2.allowance(owner2, address(this)) >= amount2,
-            "Token 2 allowance too low"
-        );
-
-        _safeTransferFrom(token1, owner1, owner2, amount1);
-        _safeTransferFrom(token2, owner2, owner1, amount2);
-    }
-
-    function _safeTransferFrom(
-        IERC20 token,
-        address sender,
-        address recipient,
-        uint amount
-    ) private {
-        bool sent = token.transferFrom(sender, recipient, amount);
-        require(sent, "Token transfer failed");
+        //pay with ERC20 token first
+        //Transfer NFTs token to user
+        safeTransferFrom(ACMToken, msg.sender, owner, amount * tokenPrice[tokenId]);
+        ACMnftToken.safeTransferFrom(owner, msg.sender, tokenId, amount, "");
     }
 }
-
-/* /Attach requestID to roller address after 
-    function GachaRandomCaller() public returns (bytes32 requestId) {
-        IERC1155 acmNFT = IERC1155(NFTContract_Address);
-        require(acmNFT.balanceOf(msg.sender, KeyId) > 0);
-        require(acmNFT.isApprovedForAll(msg.sender, address(this)), "contract must approve");
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK to pay fee");
-        require(s_results[msg.sender] == 0, "Already rolled");
-        acmNFT.safeTransferFrom(msg.sender, Owner_Address, KeyId, 1, "");
-        requestId = requestRandomness(keyHash, fee);
-        s_rollers[requestId] = msg.sender; // mapping requestId with roller address
-        s_results[msg.sender] = ROLL_IN_PROGRESS;
-        emit GachaRolled(requestId, msg.sender);
-    }
-
-
-    
-
-
-    
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
-        s_results[s_rollers[requestId]] = randomness;
-        
-    }
-
-    function openGacha() public {
-        IERC1155 acmNFT = IERC1155(NFTContract_Address);
-        require(s_results[msg.sender] != 0, "Roll a randomnumber first");
-        require(s_results[msg.sender] != ROLL_IN_PROGRESS, "Calling for random number please wait");
-
-        uint256 index = sample(itemDropChance, s_results[msg.sender]);
-        uint256 reward = rewardID[index];
-        s_results[msg.sender] = reward;
-        acmNFT.safeTransferFrom(Owner_Address, msg.sender, reward, 1, "");
-        
-        s_results[msg.sender] = 0;
-    } */
